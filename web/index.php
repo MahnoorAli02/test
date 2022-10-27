d9e0ede891ed402714e55da4d8fad02b1ab1630a
MahnoorAli02 <96840071+MahnoorAli02@users.noreply.github.com>
2022-10-27 15:55:37 +0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Welcome to Sherdil IT Academy! 333";
+echo "Welcome to Sherdil IT Academy ! 444";
 echo "\n main";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 ?>
